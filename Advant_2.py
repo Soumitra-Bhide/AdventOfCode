3a33d9d09487f64dede7aa9088d9ef271c5e42e1
Soumitra-Bhide <73633857+Soumitra-Bhide@users.noreply.github.com>
2024-12-04 21:20:10 +1100

    Update Advant_2.py

--- Advant_2.py
+++ Advant_2.py
@@ -1,20 +1,39 @@
-import os
-import collections
-
 reads=[]
+bad_reads=[]
+good_reads=[]
 
 with open("Advant_2.txt", "r") as Advant_2:
     lines = Advant_2.readlines()
     reads = [list(map(int, line.split())) for line in lines]
 
-count=0
+def safe(list,i):
+    if all(abs(j-k)<=3 for j,k in zip((list[i]),list[i][1:])):
+        if all(j > k for j,k in zip((list[i]),list[i][1:])) or all(j < k for j,k in zip((list[i]),list[i][1:])):
+            return True
+
+
 for i in range(len(reads)):
-    if all(abs(j-k)<=3 for j,k in zip((reads[i]),reads[i][1:])):
-        if all(j > k for j,k in zip((reads[i]),reads[i][1:])) or all(j < k for j,k in zip((reads[i]),reads[i][1:])):
-            print(reads[i])
-            count=count+1
-
-print(count)
-
+    if (safe(reads,i)):
+            good_reads.append(reads[i])
+    else:
+        bad_reads.append(reads[i])
             
+
+print(len(good_reads))
+
+############################################################################################################
+
+
+for i in range(len(bad_reads)):
+   sublists= [bad_reads[i][:j] + bad_reads[i][j+1:] for j in range(len(bad_reads[i]))]
+   for l in range(len(sublists)):
+       if safe(sublists,l):
+            good_reads.append(bad_reads[i])
+            break
+    
+
+print(len(good_reads))
+
+
+
         
